6c0773c84de68522ab6ac0c9ee47b8fc0b3ec4c0
storm20200 <storm20200@gmail.com>
2014-07-10 09:13:07 +0100

    Added extra error checking to ItemIDHolder
    Will now check if Item objects have a null reference to their
    itemObject GameObject.

--- Assets/Managers/Scripts/ItemIDHolder.cs
+++ Assets/Managers/Scripts/ItemIDHolder.cs
@@ -6,12 +6,17 @@ public class Item
 {
 	public GameObject itemObject;
 	public int itemID;
+	
+	
+	// Simply tests if the pointer is valid
+	public static implicit operator bool (Item item)
+	{
+		return item != null;
+	}
 }
 
 public class ItemIDHolder : MonoBehaviour 
 {
-	public Item[] ItemList;
-
 	// Use this for initialization
 	void Start () 
 	{
@@ -22,19 +27,46 @@ public class ItemIDHolder : MonoBehaviour
 		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	public Item[] ItemList;	// An array of every single available item in the game. Ideally the .itemID of each element will correspond to the index
 	
-	}
-
-	public GameObject GetItemWithID(int id)
+	
+	/// <summary>
+	/// Searches through an array of every item in the game looking for the desired itemID.
+	/// </summary>
+	/// <returns>The item with id, otherwise null.</returns>
+	/// <param name="id">ID.</param>
+	public GameObject GetItemWithID (int id)
 	{
-		foreach(Item weapon in ItemList)
+		// Check if the id corresponds to the index of ItemList then check if the item is the desired item
+		if (id >= 0 && id < ItemList.Length && 
+		    ItemList[id] && ItemList[id].itemID == id)
 		{
-			if(weapon.itemID == id)
-				return weapon.itemObject;
-		}
+			if (!ItemList[id].itemObject)
+			{
+				Debug.LogError ("An item was found in " + name + ".ItemIDHolder.ItemList with ID #" + id + " without a valid .itemObject");
+			}
 
+			return ItemList[id].itemObject;
+		}
+		
+		// Brute force it bro!
+		else
+		{
+			foreach (Item item in ItemList)
+			{
+				if (item.itemID == id)
+				{
+					if (!item.itemObject)
+					{
+						Debug.LogError ("An item was found in " + name + ".ItemIDHolder.ItemList with ID #" + id + " without a valid .itemObject");
+					}
+
+					return item.itemObject;
+				}
+			}
+		}
+		
+		// Hopefully this will never happen as it could be very costly for performance
 		Debug.Log ("Couldn't find equipment with id #" + id + ".");
 		return null;
 	}
