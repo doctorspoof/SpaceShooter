cb43e6d8895bcdf8b3b900634bc15272027a2a97
Simon Peter Campbell <storm20200@gmail.com>
2014-08-08 16:43:36 +0100

    Refactored ObjectSpin (2nd pass)

--- Assets/Scripts/Environment/ObjectSpin.cs
+++ Assets/Scripts/Environment/ObjectSpin.cs
@@ -1,54 +1,73 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-enum RotationAxis
+
+
+public sealed class ObjectSpin : MonoBehaviour
 {
-    xRot = 0,
-    yRot = 1,
-    zRot = 2
-}
+    /// <summary>
+    /// Determines the axis than an object should spin on.
+    /// </summary>
+    enum RotationAxis
+    {
+        xRot = 0,
+        yRot = 1,
+        zRot = 2
+    }
 
-public class ObjectSpin : MonoBehaviour
-{
-    [SerializeField] float SpinRate = 0;
 
-    [SerializeField] RotationAxis rotAxis = RotationAxis.zRot;
+    #region Unity modifiable variables
+    
+    [SerializeField] RotationAxis m_axisToRotateAround = RotationAxis.zRot; //!< Which axis should the object rotate around.
+    [SerializeField] float m_spinRate = 0f;                                  //!< How fast should the object spin.
 
-    Transform transf;
+    #endregion
 
+
+    #region Internal data
+
+    Transform transf = null;    //!< A cache of the transform for the sake of efficiency.
+
+    #endregion
+
+
+    #region Behaviour functions
+
+    /// <summary>
+    /// Caches the transform
+    /// </summary>
     void Awake()
     {
         transf = transform;
+       
+        transf.localPosition = CompareTag ("Star") ? new Vector3 (0, 0, 15.0f) : Vector3.zero;
     }
 
+
+    /// <summary>
+    /// Rotates around the desired axis.
+    /// </summary>
     void FixedUpdate()
     {
-        switch (rotAxis)
+        switch (m_axisToRotateAround)
         {
             case RotationAxis.xRot:
-                {
-                    //this.transform.rotation = Quaternion.Euler ( new Vector3 (this.transform.rotation.eulerAngles.x + (SpinRate * Time.deltaTime), 0, 0));
-                    transf.RotateAround(transf.position, transf.right, SpinRate * Time.deltaTime);
-                    break;
-                }
+            {
+                transf.RotateAround(transf.position, transf.right, m_spinRate * Time.deltaTime);
+                break;
+            }
             case RotationAxis.yRot:
-                {
-                    //this.transform.rotation = Quaternion.Euler ( new Vector3 (0, this.transform.rotation.eulerAngles.y + (SpinRate * Time.deltaTime), 0));
-                    transf.RotateAround(transf.position, transf.up, SpinRate * Time.deltaTime);
-                    break;
-                }
+            {
+                transf.RotateAround(transf.position, transf.up, m_spinRate * Time.deltaTime);
+                break;
+            }
             case RotationAxis.zRot:
-                {
-                    //this.transform.rotation = Quaternion.Euler ( new Vector3 (0, 0, this.transform.rotation.eulerAngles.z + (SpinRate * Time.deltaTime)));
-                    transf.RotateAround(transf.position, transf.forward, SpinRate * Time.deltaTime);
-                    break;
-                }
+            {
+                transf.RotateAround(transf.position, transf.forward, m_spinRate * Time.deltaTime);
+                break;
+            }
         }
-
-        if (this.tag == "Star")
-            transf.localPosition = new Vector3(0, 0, 15.0f);
-        else
-            transf.localPosition = Vector3.zero;
     }
+
+    #endregion
 }
 
