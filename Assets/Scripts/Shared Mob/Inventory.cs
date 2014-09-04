a2d40fdd4cfd920b541b28091eb9a48539de640a
Calum Haigh <c.haigh@gametonicinteractive.co.uk>
2014-09-04 10:30:39 +0100

    Generic inventory script beginnings

---
+++ Assets/Scripts/Shared Mob/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour 
+{
+    [SerializeField, Range(0, 20)]        int         m_maxInventorySize = 5;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
