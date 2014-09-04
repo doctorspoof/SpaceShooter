aba35b7beaef933f5e61ff6bc2dd37798b9b5e6c
Simon Peter Campbell <storm20200@gmail.com>
2014-09-04 10:41:48 +0100

    Allowed for the usage of main equipment types.

---
+++ Assets/Scripts/Equipment/EquipmentTypeEngine.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public sealed class EquipmentTypeEngine : BaseEquipment 
+{
+    protected override void CalculateCurrentStats ()
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    protected override void ElementResponseAir (int tier)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    protected override void ElementResponseFire (int tier)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    protected override void ElementResponseDark (int tier)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    protected override void ElementResponseEarth (int tier)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    protected override void ElementResponseGravity (int tier)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    protected override void ElementResponseIce (int tier)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    protected override void ElementResponseLight (int tier)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    protected override void ElementResponseLightning (int tier)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    protected override void ElementResponseOrganic (int tier)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    protected override void ElementResponseSpirit (int tier)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    protected override void ResetToBaseStats ()
+    {
+        throw new System.NotImplementedException ();
+    }
+}
