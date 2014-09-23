8eaf52143e955cfa9ee8c92cc638921651446a6b
Simon Peter Campbell <storm20200@gmail.com>
2014-09-23 15:14:25 +0100

    I'm winning

--- Assets/Scripts/Equipment/ElementalValuesEngine.cs
+++ Assets/Scripts/Equipment/ElementalValuesEngine.cs
@@ -4,109 +4,109 @@
 
 namespace ElementalValuesEngine
 {
-	/// <summary>
-	/// The Fire elemental should increase baes speed at the cost of turning and strafing ability, this is act as a drag-like enhancement.
-	/// </summary>
+    /// <summary>
+    /// The Fire elemental should increase baes speed at the cost of turning and strafing ability, this is act as a drag-like enhancement.
+    /// </summary>
     public static class Fire
     {
-        public const float  speedMulti             	= 0.2f,    	//!< Increase the engines base speed.
+        public const float  speedMulti              = 0.2f,     //!< Increase the engines base speed.
                             turnMulti               = -0.1f,    //!< Decrease ability to turn.
-                            strafeMulti           	= -0.1f;    //!< Decrease ability to strafe.
+                            strafeMulti             = -0.1f;    //!< Decrease ability to strafe.
     }
 	
 	
-	/// <summary>
-	/// Ice element increases the afterburner capacity by cooling the engines.
-	/// </summary>
+    /// <summary>
+    /// Ice element increases the afterburner capacity by cooling the engines.
+    /// </summary>
     public static class Ice
     {
-		public const float  burnerLengthMulti       = 0.1f;		//!< Increase the available capacity.
+		public const float  burnerLengthMulti       = 0.1f;     //!< Increase the available capacity.
     }
 	
 	
-	/// <summary>
-	/// Earth acts as a huge capacity upgrade at the cost of extra weight, aka lower speed.
-	/// </summary>
+    /// <summary>
+    /// Earth acts as a huge capacity upgrade at the cost of extra weight, aka lower speed.
+    /// </summary>
     public static class Earth
     {
-        public const float  burnerSpeedMulti        = -0.1f;    //!< Decrease the afterburners max speed.
-                            burnerLengthMulti       = 0.2f,     //!< Significantly increase afterburner capacity.
+        public const float  burnerSpeedMulti        = -0.1f,    //!< Decrease the afterburners max speed.
+                            burnerLengthMulti       = 0.2f;     //!< Significantly increase afterburner capacity.
     }
 	
 	
-	/// <summary>
-	/// Lightning should concentrate on increasing the afterburner speed at the cost of capacity.
-	/// </summary>
+    /// <summary>
+    /// Lightning should concentrate on increasing the afterburner speed at the cost of capacity.
+    /// </summary>
     public static class Lightning
     {
-        public const float  burnerSpeedMulti       	= 0.2f,    	//!< Make the engine go zoom zoom.
+        public const float  burnerSpeedMulti        = 0.2f,     //!< Make the engine go zoom zoom.
                             burnerLengthMulti       = -0.1f;    //!< Decrease the capacity to balance the effect.
     }
 	
 	
-	/// <summary>
-	/// Light should increase the core speed of the engine in every aspect but ultimately reduce max afterburner speed.
-	/// </summary>
+    /// <summary>
+    /// Light should increase the core speed of the engine in every aspect but ultimately reduce max afterburner speed.
+    /// </summary>
     public static class Light
     {
-        public const float  speedMulti           	= 0.1f,     //!< Increase the engines core speed.
-                            turnMulti 	        	= 0.1f,     //!< Make the engine allow for faster turning.
-                            strafeMulti				= 0.1f,		//!< Increase the agility of the engine.
-                            burnerSpeedMulti		= -0.1f;	//!< Decrease the speed of the afterburner.
+        public const float  speedMulti              = 0.1f,     //!< Increase the engines core speed.
+                            turnMulti               = 0.1f,     //!< Make the engine allow for faster turning.
+                            strafeMulti             = 0.1f,     //!< Increase the agility of the engine.
+                            burnerSpeedMulti        = -0.1f;    //!< Decrease the speed of the afterburner.
     }
 	
 	
-	/// <summary>
-	/// Dark allows for the usage of a random long-range teleport. Increased Dark element will increase the range and reduce the cooldown.
-	/// </summary>
+    /// <summary>
+    /// Dark allows for the usage of a random long-range teleport. Increased Dark element will increase the range and reduce the cooldown.
+    /// </summary>
     public static class Dark
     {
-        public const bool   longTeleport			= true;		//!< Allow for the usage of the long-range teleport.
+        public const bool   longTeleport            = true;     //!< Allow for the usage of the long-range teleport.
 
         public const float  longRangeMulti          = 0.1f,     //!< Increase the distance of the long-range teleport.
-                            longCooldownMulti 	    = -0.1f;   	//!< Decrease the cooldown of the long-range teleport.
+                            longCooldownMulti       = -0.1f;    //!< Decrease the cooldown of the long-range teleport.
     }
 	
 	
-	/// <summary>
-	/// The Spirit element allows for a controlled short-range teleport. Increased Spirit element effects the range and cooldown.
-	/// </summary>
+    /// <summary>
+    /// The Spirit element allows for a controlled short-range teleport. Increased Spirit element effects the range and cooldown.
+    /// </summary>
     public static class Spirit
     {
-        public const bool   shortTeleport        	= true;     //!< Enable the short-range teleport ability.
+        public const bool   shortTeleport           = true;     //!< Enable the short-range teleport ability.
 
         public const float  shortRangeMulti         = 0.1f,     //!< Increase the range of the teleport.
                             shortCooldownMulti      = -0.1f;    //!< Reduce the cooldown of the teleport.                                                     
     }
 	
 	
-	/// <summary>
-	/// With the Gravity element the engines can control how much planetary gravity effects them.
-	/// </summary>
+    /// <summary>
+    /// With the Gravity element the engines can control how much planetary gravity effects them.
+    /// </summary>
     public static class Gravity
     {
-        public const bool   gravityControl         	= true;     //!< Enable gravity control.
+        public const bool   gravityControl          = true;     //!< Enable gravity control.
 
-        public const float  maxGravityChangeInc   	= 0.1f ;    //!< Increase the extra gravity that can be applied to the engines.
+        public const float  maxGravityChangeInc     = 0.1f;     //!< Increase the extra gravity that can be applied to the engines.
     }
 	
 	
-	/// <summary>
-	/// Air increases the agility of the ship by a small amount with no negatives.
-	/// </summary>
+    /// <summary>
+    /// Air increases the agility of the ship by a small amount with no negatives.
+    /// </summary>
     public static class Air
     {
-        public const float  turnMulti         		= 0.1f,    	//!< Increase the turn rate of the engines.
+        public const float  turnMulti               = 0.1f,     //!< Increase the turn rate of the engines.
                             strafeMulti             = 0.1f;     //!< Increase the agility of the engines.
     }
 	
 	
-	/// <summary>
-	/// Organic is all about increasing the recharge rate of the afterburners.
-	/// </summary>
+    /// <summary>
+    /// Organic is all about increasing the recharge rate of the afterburners.
+    /// </summary>
     public static class Organic
     {
-        public const float  burnerRechargeMulti   	= 0.1f;   	//!< Increase the recharge rate of the afterburners.
+        public const float  burnerRechargeMulti     = 0.1f;     //!< Increase the recharge rate of the afterburners.
     }
 	
 	
