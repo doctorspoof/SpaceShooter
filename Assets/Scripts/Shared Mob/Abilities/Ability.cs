95bf552a321842ca892497cf2881d1bd4cb65e02
Simon Peter Campbell <storm20200@gmail.com>
2014-09-24 15:41:32 +0100

    Started work on abilities system.

---
+++ Assets/Scripts/Shared Mob/Abilities/Ability.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+
+
+/// <summary>
+/// An abstract for each ability, allows for a generic cooldown method in Ship to clean up code.
+/// </summary>
+public abstract class Ability
+{
+    float m_maxCooldown = 0.5f;     //!< How long the ability requires to cool down.
+    float m_currentCooldown = 0f;   //!< The cooldown for the ability.
+    
+    
+    public bool HasCooled()
+    {
+        return m_currentCooldown <= 0f;
+    }
+    
+    
+    public float GetCurrentCooldown()
+    {
+        return m_currentCooldown;
+    }
+    
+    
+    public float GetMaxCooldown()
+    {
+        return m_maxCooldown;
+    }
+    
+    
+    public void SetMaxCooldown (float cooldown, bool startCooldown = false)
+    {
+        m_maxCooldown = Mathf.Max (0f, cooldown);
+        
+        if (startCooldown)
+        {
+            ResetCooldown();
+        }
+    }
+    
+    
+    /// <summary>
+    /// Used to increment or decrement the current cooldown.
+    /// </summary>
+    /// <param name="alterBy">This cannot cause the current cooldown to raise above the maximum or zero.</param>
+    public void AlterCooldown (float alterBy)
+    {
+        m_currentCooldown = Mathf.Clamp (m_currentCooldown + alterBy, 0f, m_maxCooldown);
+    }
+    
+    /// <summary>
+    /// Sets the current cooldown to the maximum specified.
+    /// </summary>
+    public void ResetCooldown()
+    {
+        m_currentCooldown = m_maxCooldown;
+    }
+    
+    /// <summary>
+    /// Causes the cooldown to become zero.
+    /// </summary>
+    public void ImmediateCooldown()
+    {
+        m_currentCooldown = 0f;
+    }
+}
